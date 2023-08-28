08f12579e596ad3e1534391008071731f4f9cfa2
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2023-08-29 01:02:09 +0300

    chore: latest changes

---
+++ sketchybar/plugins/spaces/scripts/space.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+update() {
+	WIDTH="dynamic"
+	if [ "$SELECTED" = "true" ]; then
+		WIDTH="0"
+	fi
+
+	sketchybar --animate tanh 20 --set "$NAME" icon.highlight="$SELECTED" label.width="$WIDTH"
+
+}
+
+mouse_clicked() {
+	if [ "$BUTTON" = "right" ]; then
+		yabai -m space --destroy "$SID"
+		sketchybar --trigger space_change
+	else
+		echo "mouse click"
+		yabai -m space --focus "$SID" 2>/dev/null
+	fi
+}
+
+case "$SENDER" in
+"mouse.clicked")
+	mouse_clicked
+	;;
+*)
+	update
+	;;
+esac
